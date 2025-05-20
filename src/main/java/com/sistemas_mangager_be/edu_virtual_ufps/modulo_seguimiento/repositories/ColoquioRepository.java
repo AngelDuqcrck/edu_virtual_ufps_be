e1ad0306f6c1476dedbd163a1aaf4faf932153b8
Diego-Pedrozo <diegopedrozovillarreal@gmail.com>
2025-05-20 17:38:08 -0500

    feat(seguimiento): implement Email and Notificacion services for sending activity reminders

--- src/main/java/com/sistemas_mangager_be/edu_virtual_ufps/modulo_seguimiento/repositories/ColoquioRepository.java
+++ src/main/java/com/sistemas_mangager_be/edu_virtual_ufps/modulo_seguimiento/repositories/ColoquioRepository.java
@@ -1,9 +1,11 @@
 package com.sistemas_mangager_be.edu_virtual_ufps.modulo_seguimiento.repositories;
 
+import com.sistemas_mangager_be.edu_virtual_ufps.entities.Usuario;
 import com.sistemas_mangager_be.edu_virtual_ufps.modulo_seguimiento.entities.Coloquio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ColoquioRepository extends JpaRepository<Coloquio, Integer> {
@@ -18,4 +20,16 @@ public interface ColoquioRepository extends JpaRepository<Coloquio, Integer> {
     WHERE e.usuarioId.id = :usuarioId
     """)
     List<Coloquio> findColoquiosByUsuarioId(Long usuarioId);
+
+    List<Coloquio> findByFecha(LocalDate fecha);
+
+    @Query("""
+    SELECT u FROM Usuario u
+    JOIN Estudiante e ON e.usuarioId = u
+    JOIN CohorteGrupo cg ON e.cohorteId = cg
+    JOIN GrupoCohorte gc ON gc.cohorteGrupoId = cg
+    JOIN Coloquio c ON c.grupoCohorte = gc
+    WHERE c.id = :coloquioId
+    """)
+    List<Usuario> findUsuariosByColoquioId(Integer coloquioId);
 }
--- src/main/java/com/sistemas_mangager_be/edu_virtual_ufps/modulo_seguimiento/repositories/SustentacionRepository.java
+++ src/main/java/com/sistemas_mangager_be/edu_virtual_ufps/modulo_seguimiento/repositories/SustentacionRepository.java
@@ -1,10 +1,28 @@
 package com.sistemas_mangager_be.edu_virtual_ufps.modulo_seguimiento.repositories;
 
+import com.sistemas_mangager_be.edu_virtual_ufps.entities.Usuario;
 import com.sistemas_mangager_be.edu_virtual_ufps.modulo_seguimiento.entities.Sustentacion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface SustentacionRepository extends JpaRepository<Sustentacion, Integer> {
+
     Optional<Sustentacion> findByProyectoId(Integer idProyecto);
+
+    List<Sustentacion> findByFecha(LocalDate fecha);
+
+    @Query("""
+    SELECT u FROM Usuario u
+    JOIN UsuarioProyecto up ON up.usuario = u
+    JOIN Proyecto p ON up.proyecto = p
+    JOIN Sustentacion s ON s.proyecto = p
+    JOIN Rol r ON up.rol = r
+    WHERE s.id = :sustentacionId
+    AND LOWER(r.nombre) = 'estudiante'
+    """)
+    List<Usuario> findEstudiantesBySustentacionId(Integer sustentacionId);
 }
