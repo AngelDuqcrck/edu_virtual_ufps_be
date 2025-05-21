72b50e7a888dd9c0a385a1cbedf4244499854d87
Angel Duque <angelyesidducr@ufps.edu.co>
2025-05-21 10:24:43 -0500

    Implementada la recuperacion de contraseña

--- src/main/java/com/sistemas_mangager_be/edu_virtual_ufps/services/interfaces/IAdminService.java
+++ src/main/java/com/sistemas_mangager_be/edu_virtual_ufps/services/interfaces/IAdminService.java
@@ -3,6 +3,9 @@ package com.sistemas_mangager_be.edu_virtual_ufps.services.interfaces;
 import java.util.List;
 
 import com.sistemas_mangager_be.edu_virtual_ufps.exceptions.ChangeNotAllowedException;
+import com.sistemas_mangager_be.edu_virtual_ufps.exceptions.EmailNotFoundException;
+import com.sistemas_mangager_be.edu_virtual_ufps.exceptions.PasswordNotEqualsException;
+import com.sistemas_mangager_be.edu_virtual_ufps.exceptions.TokenNotValidException;
 import com.sistemas_mangager_be.edu_virtual_ufps.exceptions.UserNotFoundException;
 import com.sistemas_mangager_be.edu_virtual_ufps.shared.DTOs.AdminDTO;
 
@@ -18,4 +21,8 @@ public interface IAdminService {
 
     public void desactivarAdmin(Integer id) throws UserNotFoundException, ChangeNotAllowedException;
 
+    public void enviarTokenRecuperacion(String email) throws UserNotFoundException;
+    
+     public void restablecerpassword(String token, String nuevaPassword, String nuevaPassword2)
+            throws TokenNotValidException, EmailNotFoundException, PasswordNotEqualsException;
 }
